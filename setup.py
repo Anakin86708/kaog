0c873ac7d15411ad5214113e5c7efaf844becd31
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2021-11-23 09:25:10 -0300

    Setup.py
    Signed-off-by: Ariel Tadeu da Silva <silva.ariel2013@gmail.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("requirements.txt", "r") as fh:
+   requirements = fh.readlines()
+
 setup(
    name='kaog',
    version='0.2',
@@ -7,6 +10,5 @@ setup(
    author='Ariel Tadeu da Silva',
    author_email='silva.ariel@icloud.com',
    packages=['kaog'],
-   install_requires=['networkx', 'numpy', 'pandas', 'sklearn', 'matplotlib', 'distython'],
-   # external packages as dependencies
+   install_requires=[req.strip() for req in requirements if req[:2] != "# "],
 )
